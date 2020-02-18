70a5979db05437eeeb5e226f5ddc9e23fb031252
a.zajac@createit.com <a.zajac@createit.com>
2020-02-18 21:06:54 +0100

    add arm servos

--- rasp_tank.py
+++ rasp_tank.py
@@ -65,11 +65,8 @@ class RaspTank:
         range = self.servos['arm1'].max - self.servos['arm1'].min
         servo_value = self.servo_default + round(range*value)
         print(servo_value)
-        self.servos['arm1'].move(200)
+        self.servos['arm1'].move(servo_value)
 
-    def move_arm_2(self, value):
-        #print(self.servos['arm1'])
-        self.servos['arm1'].move(200)
 
     event_map = {
         'ABS_RZ': drive_forward,

725d0d687fff2f31f5aa3d30b979593cd9f531c5
a.zajac@createit.com <a.zajac@createit.com>
2020-02-18 20:08:19 +0100

    install adafruit module

--- servo.py
+++ servo.py
@@ -6,4 +6,8 @@ class Servo:
         self.min = min_pos
 
     def move(self, pos):
+        print('move')
         self.pwm.set_pwm(self.channel, 0, pos)
+
+    def __str__(self):
+        return self.pwm+', '+self.channel+', '+self.max+', '+self.min

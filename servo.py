3fb8c7533bd9f1cf3a8cd15de0271ca2a40216c5
a.zajac@createit.com <a.zajac@createit.com>
2020-02-18 20:09:42 +0100

    install adafruit module

--- servo.py
+++ servo.py
@@ -10,4 +10,4 @@ class Servo:
         self.pwm.set_pwm(self.channel, 0, pos)
 
     def __str__(self):
-        return self.pwm+', '+self.channel+', '+self.max+', '+self.min
+        return type(self.pwm)+', '+self.channel+', '+self.max+', '+self.min
